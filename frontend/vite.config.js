1e727c397a6626efe9b0d388846257bf49153766
chemch <cac692@gmail.com>
2025-02-05 16:00:12 -0800

    Fixed...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+// need to set the port that vite will use to the port that the server is running on
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000 
+  },
   plugins: [react()],
 })
